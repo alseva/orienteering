2eebae8e36389e248d5d13994361ced000cecd91
Aleksandra Orlova <alex.sevastyanova@gmail.com>
2022-01-08 19:16:37 +0300

    Load dfs: protocol urls and mapping yob from application config

--- app_config.py
+++ app_config.py
@@ -5,6 +5,7 @@ from openpyxl import load_workbook
 class ApplicationConfig:
     def __init__(self, excel_file: str):
         self._excel_file = excel_file
+        self._workbook = load_workbook(self._excel_file, read_only=True, keep_vba=False)
 
         self.protocol_source_type = None
         self.protocols_dir = None
@@ -13,15 +14,25 @@ class ApplicationConfig:
         self._load_main_settings()
 
         self.protocol_urls_df: pd.DataFrame = None
+        self._load_protocol_urls_df()
+
         self.mapping_yob_df: pd.DataFrame = None
+        self._load_mapping_yob_df()
 
     def _load_main_settings(self):
-        wb = load_workbook(self._excel_file, read_only=True, keep_vba=False)
-        main_settings = dict(wb['Настройки приложения'].values)
+        main_settings = dict(self._workbook['Настройки приложения'].values)
         self.protocol_source_type = main_settings['Тип источника протоколов']
         self.protocols_dir = main_settings['Путь к папке со всеми протоколами']
         self.race_of_the_top_protocols_dir = main_settings['Путь к папке с протоколами для гонки сильнейших']
         self.previous_year_final_rank_file = main_settings['Путь к файлу с финальным рангом предыдущего года']
+
+    def _load_protocol_urls_df(self):
+        protocol_urls = list(self._workbook['Ссылки на протоколы'].values)
+        self.protocol_urls_df = pd.DataFrame(protocol_urls[1:], columns=protocol_urls[0])
+
+    def _load_mapping_yob_df(self):
+        mapping_yob = list(self._workbook['Маппинг. Год рождения'].values)
+        self.mapping_yob_df = pd.DataFrame(mapping_yob[1:], columns=mapping_yob[0])
 
 
 if __name__ == '__main__':
