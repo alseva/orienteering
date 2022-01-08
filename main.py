5979cf4a5f2bfc2924c0fe13fa3809ab986fbd18
Aleksandra Orlova <alex.sevastyanova@gmail.com>
2022-01-08 19:40:08 +0300

    Create run template

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+from app_config import ApplicationConfig
+from rank_formula_config import RankFormulaConfig
+
+
+def main():
+    application_config = ApplicationConfig('Конфигуратор приложения.xlsx')
+    rank_formula_config = RankFormulaConfig()
+
+    protocols_df = load_protocols(application_config)
+    current_rank_df = calculate_current_rank(rank_formula_config, protocols_df)
+    save_current_rank(application_config, current_rank_df)
+
+
+def load_protocols(application_config: ApplicationConfig) -> pd.DataFrame:
+    return pd.DataFrame()
+
+
+def calculate_current_rank(rank_formula_config: RankFormulaConfig, protocols_df: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame()
+
+
+def save_current_rank(application_config: ApplicationConfig, current_rank_df: pd.DataFrame):
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ rank_formula_config.py
@@ -0,0 +1,2 @@
+class RankFormulaConfig:
+    pass
